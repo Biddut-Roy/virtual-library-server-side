cb9f9616389eeac849d628e866f929dd33874adc
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2023-11-05 23:47:36 +0600

    all books and borrowed books get link created

--- index.js
+++ index.js
@@ -56,25 +56,60 @@ async function run() {
 
         // Books collection
         const booksData = client.db("library").collection("books");
+        app.get("/addBook", async (req, res) => {
+            const result = await booksData.find().toArray();
+            res.send(result);
+        });
+
         app.get('/categorybooks/:category', async (req, res) => {
             const category = req.params.category;
-            const query = { category: category}
+            const query = { category: category }
             const result = await booksData.find(query).toArray();
             res.send(result);
         });
 
         app.get("/details/:id", async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await booksData.findOne(query)
             res.send(result);
         });
+
         app.post('/books', async (req, res) => {
             const body = req.body;
             const result = await booksData.insertOne(body);
-            console.log(body);
             res.send(result);
         })
+
+        //  update quantity
+
+        app.patch('/item-update/:id', async (req, res) => {
+            const id = req.params.id;
+            const body = req.body;
+            console.log(body.qnt);
+            const filter = { _id: new ObjectId(id)};
+            const update = {
+                $set:{
+                    quantity: body.qnt,
+                }
+            }
+            const result = await booksData.updateOne(filter, update);
+
+            res.send(result);
+        })
+
+        //  My cards (CURD operation)
+        const borrowData = client.db("library").collection("borrow");
+        app.get('/borrow', async (req, res) => {
+            const result = await borrowData.find().toArray();
+            res.send(result);
+        });
+
+        app.post('/borrow', async (req, res) => {
+            const body = req.body;
+            const result = await borrowData.insertOne(body);
+            res.send(result);
+        });
 
 
         await client.db("admin").command({ ping: 1 }); // server update time this line delete

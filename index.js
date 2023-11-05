84a290840eef13415466211782625efd3a18dabc
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2023-11-05 17:54:23 +0600

    id base book detais get api setup complete

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 require('dotenv').config();
 const cors = require('cors')
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors())
@@ -58,9 +58,15 @@ async function run() {
         const booksData = client.db("library").collection("books");
         app.get('/categorybooks/:category', async (req, res) => {
             const category = req.params.category;
-            console.log(category);
             const query = { category: category}
             const result = await booksData.find(query).toArray();
+            res.send(result);
+        });
+
+        app.get("/details/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id)}
+            const result = await booksData.findOne(query)
             res.send(result);
         });
         app.post('/books', async (req, res) => {

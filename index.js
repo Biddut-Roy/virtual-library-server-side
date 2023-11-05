52374a1f6ec7614f65e1d07eadc1904d563adee2
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2023-11-05 16:35:43 +0600

    category base book show get link create

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 
@@ -19,52 +19,68 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
+    try {
 
-    await client.connect(); // server update time this line delete
+        await client.connect(); // server update time this line delete
 
-    // user
-    const userData = client.db("library").collection("user");
-    app.post('/user' , async (req, res) => {
-        const body = req.body;
-        console.log(body);
-        const result = await userData.insertOne(body);
-        res.send(result);
-    })
+        // user
+        const userData = client.db("library").collection("user");
+        app.post('/user', async (req, res) => {
+            const body = req.body;
+            console.log(body);
+            const result = await userData.insertOne(body);
+            res.send(result);
+        })
 
-    // category
-    const categoryData = client.db("library").collection("category");
-    app.get('/category' , async(req, res )=>{
-        const result = await categoryData.find().toArray();
-        res.send(result);
-    })
+        // category
+        const categoryData = client.db("library").collection("category");
+        app.get('/category', async (req, res) => {
+            const result = await categoryData.find().toArray();
+            res.send(result);
+        })
 
-    // writer
-    const writerData = client.db("library").collection("writer");
-    app.get('/writer' , async(req, res)=>{
-        const result = await writerData.find().toArray();
-        res.send(result);
-    })
+        // writer
+        const writerData = client.db("library").collection("writer");
+        app.get('/writer', async (req, res) => {
+            const result = await writerData.find().toArray();
+            res.send(result);
+        })
+
+        // Books collection
+        const booksData = client.db("library").collection("books");
+        app.get('/categorybooks/:category', async (req, res) => {
+            const category = req.params.category;
+            console.log(category);
+            const query = { category: category}
+            const result = await booksData.find(query).toArray();
+            res.send(result);
+        });
+        app.post('/books', async (req, res) => {
+            const body = req.body;
+            const result = await booksData.insertOne(body);
+            console.log(body);
+            res.send(result);
+        })
 
 
-    await client.db("admin").command({ ping: 1 }); // server update time this line delete
-    console.log("Pinged your deployment. You successfully connected to MongoDB!"); // server update time this line delete
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        await client.db("admin").command({ ping: 1 }); // server update time this line delete
+        console.log("Pinged your deployment. You successfully connected to MongoDB!"); // server update time this line delete
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
